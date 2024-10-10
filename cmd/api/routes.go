5523a957889aede0c74e7bf1e63effeb96a6cf19
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 14:07:14 +0100

    Update routes to be application pointer receiver function

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,12 +2,12 @@ package main
 
 import "net/http"
 
-func routes() http.Handler {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /v1/posts", getAllPost)
-	mux.HandleFunc("POST /v1/posts", createNewPost)
-	mux.HandleFunc("DELETE /v1/posts/{id}", deletePost)
+	mux.HandleFunc("GET /v1/posts", app.getAllPost)
+	mux.HandleFunc("POST /v1/posts", app.createNewPost)
+	mux.HandleFunc("DELETE /v1/posts/{id}", app.deletePost)
 
 	return mux
 }
