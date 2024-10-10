95e0036956b7dd1074707da51bc4a52fe3e97b3e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 11:42:50 +0100

    Add routes.go to map the handlers to specific routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/posts", getAllPost)
+	mux.HandleFunc("POST /v1/posts", createNewPost)
+	mux.HandleFunc("DELETE /v1/posts/:id", deletePost)
+
+	return mux
+}
