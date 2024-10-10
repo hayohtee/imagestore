39cdb0448c8eae24c12b6fd8ea9808dd223aa179
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 14:06:49 +0100

    Configure aws sdk and s3 client

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/joho/godotenv"
 )
 
-func main()  {
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	awsConfig, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion(os.Getenv("AWS_BUCKET_REGION")),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(os.Getenv("AWS_ACCESS_KE"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")),
+	)
+
+	app := application{
+		s3Client: s3.NewFromConfig(awsConfig),
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	server := http.Server{
-		Addr: ":4000",
-		ReadTimeout: 10 * time.Second,
+		Addr:         ":4000",
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
-		Handler: routes(),
+		Handler:      app.routes(),
 	}
 
 	log.Println("listening on :4000")
-	err = server.ListenAndServe(); if err != nil {
+	err = server.ListenAndServe()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
