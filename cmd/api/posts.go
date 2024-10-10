32bf51ed09c6e131c828a2deaadf55f2470c8e2a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 14:04:42 +0100

    Update handlers to be method on application struct

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,15 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-func createNewPost(w http.ResponseWriter, r *http.Request) {
+func (app *application) createNewPost(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10485760)
+	log.Println(r.FormValue("name"))
+	log.Println(r.FormValue("email"))
+
+	_, header, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "error parsing getting image from the form-data", http.StatusUnprocessableEntity)
+		return
+	}
+	log.Println(header.Filename)
+	log.Println(r.FormValue("caption"))
+}
+
+func (app *application) deletePost(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deletePost(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func getAllPost(w http.ResponseWriter, r *http.Request) {
+func (app *application) getAllPost(w http.ResponseWriter, r *http.Request) {
 
 }
