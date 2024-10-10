f76d319b852f4d0f47d38f1a474c907b938aa8ad
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 11:43:03 +0100

    Add main.go to setup the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
 func main()  {
-	
+	server := http.Server{
+		Addr: ":4000",
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Handler: routes(),
+	}
+
+	log.Println("listening on :4000")
+	err := server.ListenAndServe(); if err != nil {
+		log.Fatal(err)
+	}
 }
