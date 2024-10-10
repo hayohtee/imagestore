4dcb054f5a4abd7d1f8ba4c8c1efcffa1443f50a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 14:25:49 +0100

    Add the code to upload image to s3 bucket

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func (app *application) createNewPost(w http.ResponseWriter, r *http.Request) {
@@ -10,13 +17,30 @@ func (app *application) createNewPost(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.FormValue("name"))
 	log.Println(r.FormValue("email"))
 
-	_, header, err := r.FormFile("image")
+	file, header, err := r.FormFile("image")
 	if err != nil {
 		http.Error(w, "error parsing getting image from the form-data", http.StatusUnprocessableEntity)
 		return
 	}
-	log.Println(header.Filename)
-	log.Println(r.FormValue("caption"))
+	defer file.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+	
+	_, err = app.s3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("AWS_BUCKET_NAME")),
+		Key: aws.String(header.Filename),
+		Body: file,
+		ContentType: aws.String("images/*"),
+	})
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, fmt.Sprintf("could not upload %s", header.Filename), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("image uploaded successfully"))
 }
 
 func (app *application) deletePost(w http.ResponseWriter, r *http.Request) {
