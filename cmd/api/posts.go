eab03aa3f2c887aad994cccdac1421112e183221
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-10 11:42:27 +0100

    Add posts.go to hold the handlers for the api

---
+++ cmd/api/posts.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func createNewPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getAllPost(w http.ResponseWriter, r *http.Request) {
+
+}
