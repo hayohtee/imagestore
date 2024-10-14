f36e121e025a03191186f1c6fcdc57fef3849d0f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-14 12:12:45 +0100

    Add helper functions for generating random string, filename, file extension

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"crypto/rand"
+	"errors"
+	"fmt"
+	"mime"
+	"path/filepath"
+	"strings"
 )
 
-func generateRandomBytes(n int) ([]byte, error) {
+func generateRandomString(n int) (string, error) {
 	randBytes := make([]byte, n)
 	_, err := rand.Read(randBytes)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return randBytes, nil
+	// Convert bytes to an hexadecimal string
+	return fmt.Sprintf("%x", randBytes), nil
+}
+
+func getFileExtension(filename, contentType string) string {
+	// If contentType is available, derive extension from it
+	if contentType != "" {
+		exts, err := mime.ExtensionsByType(contentType)
+		if err == nil && len(exts) > 0 {
+			return exts[0]
+		}
+	}
+	// If the contentType is not specified, fallback to using the filename
+	return strings.ToLower(filepath.Ext(filename))
+}
+
+func generateUniqueFilename(filename, contentType string) (string, error) {
+	randomString, err := generateRandomString(16)
+	if err != nil {
+		return "", err
+	}
+
+	fileExtension := getFileExtension(filename, contentType)
+	if fileExtension == "" {
+		return "", errors.New("unable to generate file extension")
+	}
+
+	uniqueFileName := randomString + fileExtension
+	return uniqueFileName, nil
 }
